8d613e175c7d97d9f24797677e56d77a9934555d
Benny Joram <80184074+borvux@users.noreply.github.com>
2023-11-24 02:46:25 +0000

    used hash for encoder.rb

--- ruby_gym/encoder.rb
+++ ruby_gym/encoder.rb
@@ -13,10 +13,24 @@ secret = [
   "we should be more clever"
 ].sample
 pp secret
+
 # write your program below
+=begin
 secret = secret.gsub("a", "1").gsub("A", "1") 
 secret = secret.gsub("e", "2").gsub("E", "2")
 secret = secret.gsub("i", "3").gsub("I", "3")
 secret = secret.gsub("o", "4").gsub("O", "4")
 secret = secret.gsub("u", "5").gsub("U", "5")
+pp secret
+=end
+
+#use hash to map each char with num
+mapping = {
+  'a' => '1', 'e' => '2', 'i' => '3', 'o' => '4', 'u' => '5',
+  'A' => '1', 'E' => '2', 'I' => '3', 'O' => '4', 'U' => '5'
+}
+#loop and sub the char with num
+mapping.each { |char, num| 
+secret.gsub!(char, num) 
+}
 pp secret
